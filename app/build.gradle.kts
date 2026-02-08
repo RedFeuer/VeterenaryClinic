47f1bf305f0b771087017854b62016d705b27c6e
RedFeuer <danilabespyatov@gmail.com>
2026-02-08 09:41:08 +0300

    DEPENDENCIES: Hilt + KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+
+    // Hilt runtime + compiler for KSP
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -50,6 +54,13 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.room.ktx)
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+
+
+    // Hilt runtime + compiler for KSP
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
